76d74c129e583c567d27708fbc1fc8af9ad9c6d7
Alex Nataf <alexnataf@NOTSODARKs-MacBook-Pro.local>
2024-10-17 19:07:14 +0200

    Add apostrophe dipslay function

--- utils.py
+++ utils.py
@@ -5,3 +5,8 @@ def convert_to_int(value):
     if pd.isna(value) or value == '':
         return ''
     return int(value)
+
+def format_time(value):
+    if pd.isna(value) or value == '':
+        return ''
+    return f"{convert_to_int(value)}'"  
